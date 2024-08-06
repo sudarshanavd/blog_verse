c0d8d8d3a2767bc7c3f79216d7a391cee103d9bc
sudarshanavd <sudarshannaikru@gmail.com>
2024-08-06 08:05:48 +0530

    user templates added

--- blogverse/urls.py
+++ blogverse/urls.py
@@ -26,27 +26,27 @@ urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
     path(
-        "password_change/", views.PasswordChangeView.as_view(), name="password_change"
+        "password_change/", views.PasswordChangeView.as_view(template_name='user/password_change_form.html'), name="password_change"
     ),
     path(
         "password_change/done/",
-        views.PasswordChangeDoneView.as_view(),
+        views.PasswordChangeDoneView.as_view(template_name='user/password_change_done.html'),
         name="password_change_done",
     ),
-    path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/", views.PasswordResetView.as_view(template_name='user/password_reset_form.html'), name="password_reset"),
     path(
         "password_reset/done/",
-        views.PasswordResetDoneView.as_view(),
+        views.PasswordResetDoneView.as_view(template_name='user/password_reset_done.html'),
         name="password_reset_done",
     ),
     path(
         "reset/<uidb64>/<token>/",
-        views.PasswordResetConfirmView.as_view(),
+        views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'),
         name="password_reset_confirm",
     ),
     path(
         "reset/done/",
-        views.PasswordResetCompleteView.as_view(),
+        views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'),
         name="password_reset_complete",
     ),
 ]
